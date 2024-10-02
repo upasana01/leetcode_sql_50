31fbe67fdd57c79eeeeebc58b1bcbe2b300de240
Upasana Chaudhari <37362186+upasana01@users.noreply.github.com>
2024-10-02 09:44:50 -0700

    Update 1148. Article Views I.sql

--- select/1148. Article Views I.sql
+++ select/1148. Article Views I.sql
@@ -43,3 +43,12 @@ Output:
 | 4    |
 | 7    |
 +------+
+
+
+Answer:
+
+# Write your MySQL query statement below
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by id asc;
